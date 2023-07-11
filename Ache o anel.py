51ca5aef79e923afc0a78e4c54a42f542c9e7ae4
Abel Lucas <abelucas009@gmail.com>
2023-07-11 12:23:34 -0300

    alteração na sprites dos botões dos modos de jogo

--- Ache o anel.py
+++ Ache o anel.py
@@ -15,12 +15,10 @@ class Object(object):
 
 class Game:
     def __init__(self):
+        pyxel.init(90,140,"Ache O Anel")
         #Variaveis inciais
         self.play= False
         self.chose_mode= False
-        
-        pyxel.init(90,140,"Ache O Anel")
-        
         #objetos
         self.gameHand=Object(3,44,1,0,0,40,93)
         self.gameCup= Object(pyxel.width-43,44,1,0,93,40,93)
@@ -41,6 +39,7 @@ class Game:
                             btn.y= 43
                             if pyxel.btn(pyxel.MOUSE_BUTTON_LEFT):
                                 btn.y= 44
+                                btn.imgx= 80
                     else: 
                         btn.imgx= 0
                         btn.y= 44 
@@ -61,14 +60,13 @@ class Game:
                 pyxel.blt(0,0,0,90,0,90,140)
                 for btn in self.modeList: btn.draw()
                 pyxel.text(pyxel.width/2 - len("Escolha um modo:")/2 *4,37,"Escolha um modo:",7)
+            #if modo mão
+            #if modo copo
                 
         #Menu Inicial
         else:
             pyxel.blt(0,0,0,0,0,90,140)
             pyxel.text(pyxel.width/2 - len("Clique para continuar")/2 *4,133,"Clique para continuar",7)#pyxel.frame_count %16)
-
-            #if modo mão
-            #if modo copo
 
 #Verificação da execução direta do módulo
 if __name__ == "__main__":
