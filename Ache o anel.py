b133d9b6b2458bd8264cff3042a4d58e55cd4a3b
Abel Lucas <abelucas009@gmail.com>
2023-07-14 20:59:39 -0300

    add textos para acertos e erros
    falta:
    - mostrar o total  de scores feito
    -poder voltar ao menu inicial

--- Ache o anel.py
+++ Ache o anel.py
@@ -24,7 +24,6 @@ class Object(object):
         if pyxel.mouse_x >= self.x+x1 and pyxel.mouse_x <= self.x + self.w+x2 \
             and pyxel.mouse_y >= self.y+y1 and pyxel.mouse_y <= self.y + self.h+y2:
                 self.mouseUp=True
-
                 if pyxel.btn(pyxel.MOUSE_BUTTON_LEFT):
                     self.mouseClick=True
                 else:self.mouseClick=False
@@ -36,7 +35,6 @@ class Object(object):
                 if pyxel.btnr(pyxel.MOUSE_BUTTON_LEFT):
                     self.mouseRealesed=True
                 else:self.mouseRealesed=False
-
         else:self.mouseUp=False
     
     def draw(self):
@@ -60,11 +58,14 @@ class Game:
         #Declarando variaveis dos modos de jogos
         #Variaveis do modo GAME HAND
         self.scores = 0
+        self.correctHand= 1
+        self.confetti_imgx,self.confetti_imgy=0,72
         self.rightHand= Object(10,50,2,0,0,32,32)#13
         self.leftHand= Object(42,50,2,0,32,32,32)#45
         self.handsList= [self.rightHand,self.leftHand]
-        
+
         pyxel.load("resources/Ache_O_Anel.pyxres")
+        #pyxel.playm(0, loop=True)
         pyxel.run(self.update,self.draw) 
         
     def update(self):
@@ -97,26 +98,39 @@ class Game:
                 for hand in self.handsList:
                     self.rightHand.verClick(x1=9, x2=-5, y1=0, y2=-7)
                     self.leftHand.verClick(x1=4, x2=-10,y2=-7)
-                    
                     if hand.mouseUp: 
                         hand.imgx= 32
-
+                        
                         if hand.mouseClick:
-                            hand.imgx= 96 if hand.ring else 64
+                            hand.imgx= 96 if hand.ring else 64      
                         
                         if hand.mousePressed:
-                            self.scores= self.scores+1 if hand.ring else 0
-                        
+                            if hand.ring:
+                                self.scores+=1 
+                                self.correctHand= 2
+                                
+                            else:
+                                self.scores= 0
+                                self.correctHand=0
+
                         if hand.mouseRealesed: 
                             hand.ring=pyxel.rndi(0,1)
+                    else: 
+                        hand.imgx= 0
 
-                    else: hand.imgx= 0
-                            
                 #Evitando que o anel surja nas 2 mãos
                 if self.rightHand.ring == self.leftHand.ring:
                     self.rightHand.ring=pyxel.rndi(0,1)
                     self.leftHand.ring= pyxel.rndi(0,1)
-
+                    
+                #Animação dos confetes
+                if self.confetti_imgy < 96:
+                    if self.confetti_imgx< 224:
+                        self.confetti_imgx+=32
+                    else:
+                        self.confetti_imgy= 96
+                        self.confetti_imgx=0
+                else: self.confetti_imgy=64
         #Menu Inicial
         else:
             if pyxel.btnr(pyxel.MOUSE_BUTTON_LEFT):
@@ -140,10 +154,15 @@ class Game:
                 for hand in self.handsList:
                     hand.draw()
 
+                    if self.correctHand== 2:
+                        pyxel.blt(-7,-5,2,self.confetti_imgx,self.confetti_imgy, 32,32)         
+                        pyxel.blt(100-37,-5,2,self.confetti_imgx,self.confetti_imgy, -32,32)
+                        pyxel.text(pyxel.width/2- len("Acertou!")/2 *4, 20,"Acertou!",pyxel.frame_count %16)
+                    if self.correctHand== 0: 
+                        pyxel.text(pyxel.width/2-len("Errou!")/2 *4, 20,"Errou!",7)
         else:
             pyxel.blt(0,0,0,0,0,90,140)
             pyxel.text(pyxel.width/2 - len("Clique para continuar")/2 *4,133,"Clique para continuar",7)
-            
 #Verificação da execução direta do módulo
 if __name__ == "__main__":
     Game()
